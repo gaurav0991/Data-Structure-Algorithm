c3a8d301aaad9f5db299b750ecb77f0e70e50cad
Gaurav Saraf <gsaraf895@gmail.com>
2020-11-02 02:04:25 +0530

    first commit

---
+++ sort/mergesort/mergeSort.js
@@ -0,0 +1,38 @@
+function mergeSort(arr){
+    if(arr.length<=1){
+        return arr;
+    }
+    let mid=Math.floor(arr.length/2);
+    let left=mergeSort(arr.slice(0,mid));
+    let right=mergeSort(arr.slice(mid));
+    return merge(left,right)
+    
+}
+function merge(arr1,arr2){
+    let i=0;
+    let j=0;
+    let res=[];
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]<arr2[j]){
+            res.push(arr1[i]);
+            i++;
+        }else{
+            res.push(arr2[j]);
+            j++;
+
+        }
+    }
+    while(i<arr1.length){
+        res.push(arr1[i]);
+        i++;
+    }
+    
+    while(j<arr2.length){
+        res.push(arr2[j]);
+        j++;
+    }
+    return res;
+}
+let arr=[10,22,8,1,7]
+let a=mergeSort(arr)
+console.log(a)
